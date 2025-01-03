c74dfb961133c33b4d66c00c0650db58a603981d
rahul reddy <dhanapanarahulreddy4444@gmail.com>
2025-01-03 19:36:14 +0530

    add particle animation to body of a page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Particles from "react-tsparticles";
+import { loadFull } from 'tsparticles'; 
 import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact'; 
@@ -7,11 +9,17 @@ import Skills from './pages/skills';
 import Projects from './pages/projects';
 import Resume from './pages/resume';
 import Navbar from './components/navbar';
+import particles from './utils/particles';
 
 function App() {
+  const handleInit = async (main)=>{
+    await loadFull(main);
+  }
+
   return (
     <div className="App">
       {/* particle js */}
+      <Particles id="particles" options={particles} init={handleInit}/>
 
       {/* navbar */}
       <Navbar/>
