25871c253dbc598f986a4a2df572c6f1d16fc8ad
rahul reddy <dhanapanarahulreddy4444@gmail.com>
2025-01-09 22:26:30 +0530

    add animated skills section with categorized progress bars

---
+++ src/pages/skills/utils.js
@@ -0,0 +1,57 @@
+export const skillsData = [
+    {
+        label: "Languages",
+        data: [
+            {
+                skillName: "C++",
+                percentage: "90",
+            },
+            {
+                skillName: "Python",
+                percentage: "70",
+            },
+            {
+                skillName: "JavaScript",
+                percentage: "65",
+            },
+        ],
+    },
+    
+    {
+        label: "Frontend",
+        data: [
+            {
+                skillName: "ReactJs",
+                percentage: "75",
+            },
+        ],
+    },
+
+    {
+        label: "Backend",
+        data: [
+            {
+                skillName: "Node.js",
+                percentage: "75"
+            },
+            {
+                skillName: "Express.js",
+                percentage: "75"
+            },
+        ],
+    },
+
+    {
+        label: "Databases",
+        data: [
+            {
+                skillName: "MongoDB",
+                percentage: "85"
+            },
+            {
+                skillName: "SQL",
+                percentage: "90"
+            },
+        ],
+    },
+];
