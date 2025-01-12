8cea0ba2ffa91525e17582b7a219ff29971de69b
rahul reddy <dhanapanarahulreddy4444@gmail.com>
2025-01-12 16:30:32 +0530

    Add education and achievements section with timeline animation

--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import {BsInfoCircleFill} from "react-icons/bs";
 import PageHeaderContent from "../../components/pageHeaderContent";
+import {VerticalTimeline, VerticalTimelineElement} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import {data} from "./utils";
+import "./style.scss";
+import {MdWork} from 'react-icons/md'
+
 
 const Resume=()=>{
     return(
@@ -8,6 +14,68 @@ const Resume=()=>{
             <section className="resume" id="resume">
             <PageHeaderContent headerText = "My Resume" icon = {<BsInfoCircleFill size={40}/>}/>
             </section>
+            <div className="timeline">
+            <div className="timeline_education">
+                    <h3 className="timeline_education_header-text">Education</h3>
+                    <VerticalTimeline layout={'1-column'} lineColor="var( --yellow-theme-main-color)">
+                        {
+                            data.education.map((item,i) =>(
+                                <VerticalTimelineElement key={i} className="timeline_experience_vertical-timeline-element" 
+                                contentStyle={{background: 'none', color : 'var(--yellow-theme-sub-text-color)', border: '1.5px solid var(--yellow-theme-main-color)'}}
+                                date= {item.date}
+                                icon = {<MdWork/>}
+                                iconStyle={{
+                                    background: '#181818',
+                                    color: 'var(--yellow-theme-main-color)'
+                                }}
+                                >
+                                    <div className="vertical-timeline-element-title-wrapper">
+                                        <h3>
+                                            {item.title}
+                                        </h3>
+                                        
+                                        <h4>
+                                            {item.subTitle}
+                                        </h4>
+                                    </div>
+                                    <p className="vertical-timeline-element-title-wrapper-description">{item.description}</p>
+                                </VerticalTimelineElement>
+                            ))
+                        }
+                    </VerticalTimeline>
+                </div>
+                <div className="timeline_experience">
+                    <h3 className="timeline_experience_header-text">Achivements</h3>
+                    <VerticalTimeline layout={'1-column'} lineColor="var( --yellow-theme-main-color)">
+                        {
+                            data.experience.map((item,i) =>(
+                                <VerticalTimelineElement key={i} className="timeline_experience_vertical-timeline-element"
+                                contentStyle={{background: 'none', color : 'var(--yellow-theme-sub-text-color)', border: '1.5px solid var(--yellow-theme-main-color)'}} 
+                                date= {item.date}
+                                icon = {<MdWork/>}
+                                iconStyle={{
+                                    background: '#181818',
+                                    color: 'var(--yellow-theme-main-color)'
+                                }}
+                                >
+                                    <div className="vertical-timeline-element-title-wrapper">
+                                        <h3>
+                                            {item.title}
+                                        </h3>
+                                        
+                                        <h4>
+                                            {item.subTitle}
+                                        </h4>
+                                    </div>
+                                    <p className="vertical-timeline-element-title-wrapper-description">{item.description}</p>
+
+                                </VerticalTimelineElement>
+                            ))
+
+                        }
+                    </VerticalTimeline>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/pages/resume/utils.js
@@ -0,0 +1,38 @@
+export const data = {
+    experience: [
+        {
+            title:"AVISHKAR",
+            // subTitle: "-competitive programming",
+            description: "Achieved Top 10 in the final round of Softathlon, a competitive programming event with 400 participants.",
+            date: "2024"
+        },
+        {
+            title: "Creamy Olympiad Test",
+            // subTitle: "",
+            description: "Secured Zonal Rank 1 in the School Creamy Olympiad Test",
+            date : "2019"
+        },
+    ],
+
+    education: [
+        {
+            title: " Motilal Nehru National Institute Of Technology,Allahabad",
+            // subTitle: "",
+            description: "B.Tech, Computer Science Engineering ",
+            date: "2022-2026"
+
+        },
+        {
+            title: "Sanjeevi Junior Collage",
+            // subTitle: "",
+            description: "Board of Intermediate Education, Andhra Pradesh (Intermediate)",
+            date: "2020-2022",
+        },
+        {
+            title: " Ravindra Bharathi Global School",
+            // subTitle: "",
+            description: "Board of Secondary Education, Andhra Pradesh (High School)",
+            date: "2019-2020"
+        }, 
+    ],
+}
